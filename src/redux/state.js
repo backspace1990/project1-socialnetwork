0d2d74971be53fccd1ea14770064bd1f20ba9c79
backspace1990 <umut90asan@gmail.com>
2021-02-24 02:45:22 +0300

    ilk redux dosyamizi olusturduk. burada sabit bilgiler suan yer aliyor

---
+++ src/redux/state.js
@@ -0,0 +1,32 @@
+
+
+let state = {
+    profilePage:{
+        posts: [
+            {id: 1, message: 'Hi, how are you?', likesCount: '12'},
+            {id: 2, message: 'It\'s my first post.', likesCount: '30'},
+            {id: 1, message: 'Blabla?', likesCount: '35'},
+            {id: 2, message: 'Da da!!', likesCount: '67'}
+        ]
+    },
+    dialogsPage:{
+        messagesData: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How is your it-kamasutra?'},
+            {id: 3, message: 'Yo'},
+            {id: 4, message: 'Yo'},
+            {id: 5, message: 'Yo'},
+            {id: 6, message: 'Yo'}
+        ],
+        dialogsData: [
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Andrey'},
+            {id: 3, name: 'Sveta'},
+            {id: 4, name: 'Sasha'},
+            {id: 5, name: 'Viktor'},
+            {id: 6, name: 'Valera'},
+            {id: 7, name: 'Umit'},
+        ]
+    }
+};
+export default state;
