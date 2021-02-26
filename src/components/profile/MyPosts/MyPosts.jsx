f1f9d641c17fea6e1647074407328343fd078f7c
backspace1990 <umut90asan@gmail.com>
2021-02-26 23:17:54 +0300

    render.js silinmesi sonrasi MyPosts.jsx dosyasindaki duzenlemeler yapildi

--- src/components/profile/MyPosts/MyPosts.jsx
+++ src/components/profile/MyPosts/MyPosts.jsx
@@ -5,15 +5,25 @@ import Post from "./Post/Post";
 const MyPosts = (props) => {
     let postsElements = props.posts.map(post => <Post message={post.message} like={post.likesCount}/>
     );
+    let newPostElement = React.createRef();
+    let addPost = () => {
+        props.addPost();
+    };
+    let onPostChange = () => {
+        let text = newPostElement.current.value;
+        props.updateNewPostText(text);
+
+
+    }
     return (
         <div className={classes.postBlock}>
             <h3>My post</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}/>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                     <button>Remove</button>
                 </div>
             </div>
