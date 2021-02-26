06b234ae8ef03532381b13ec004c140679ef83ae
backspace1990 <umut90asan@gmail.com>
2021-02-26 23:12:13 +0300

    render.js silindikten sonra app.js dosyasi duzenlendi

--- src/App.js
+++ src/App.js
@@ -17,8 +17,11 @@ const App = (props) => {
                 <Header/>
                 <Navbar/>
                 <div className='site-wrapper-content'>
-                    <Route path="/dialogs" render={() => <Dialogs state={props.state.dialogsPage}/>}/>
-                    <Route path="/profile" render={() => <Profile state={props.state.profilePage}/>}/>
+                    <Route path="/dialogs"
+                           render={() => <Dialogs state={props.state.dialogsPage} addMessage={props.addMessage}/>}/>
+                    <Route path="/profile"
+                           render={() => <Profile profilePage={props.state.profilePage} addPost={props.addPost}
+                                                  updateNewPostText={props.updateNewPostText}/>}/>
                     <Route path="/news" render={() => <News/>}/>
                     <Route path="/music" render={() => <Music/>}/>
                     <Route path="/settings" render={() => <Settings/>}/>
