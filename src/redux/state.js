c612a2fc6a59ce73ead2d6cc4bac1e1071fbc038
backspace1990 <umut90asan@gmail.com>
2021-03-01 12:43:39 +0300

    Object Oriented Programming(OOP) Nesne yonelimli programlama sekline degistirildi!

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,63 +1,71 @@
-let rendererEntireTree = () => {
-    console.log('state was changed');
-}
-
-let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likesCount: '12'},
-            {id: 2, message: 'It\'s my first post.', likesCount: '30'},
-            {id: 1, message: 'Blabla?', likesCount: '35'},
-            {id: 2, message: 'Da da!!', likesCount: '67'}
-        ],
-        newPostText: 'it-kamasutra.com'
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi, how are you?', likesCount: '12'},
+                {id: 2, message: 'It\'s my first post.', likesCount: '30'},
+                {id: 1, message: 'Blabla?', likesCount: '35'},
+                {id: 2, message: 'Da da!!', likesCount: '67'}
+            ],
+            newPostText: 'it-kamasutra.com'
+        },
+        dialogsPage: {
+            messagesData: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How is your it-kamasutra?'},
+                {id: 3, message: 'Yo'},
+                {id: 4, message: 'Yo'},
+                {id: 5, message: 'Yo'},
+                {id: 6, message: 'Yo'}
+            ],
+            dialogsData: [
+                {id: 1, name: 'Dimych'},
+                {id: 2, name: 'Andrey'},
+                {id: 3, name: 'Sveta'},
+                {id: 4, name: 'Sasha'},
+                {id: 5, name: 'Viktor'},
+                {id: 6, name: 'Valera'},
+                {id: 7, name: 'Umit'},
+            ]
+        }
     },
-    dialogsPage: {
-        messagesData: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'How is your it-kamasutra?'},
-            {id: 3, message: 'Yo'},
-            {id: 4, message: 'Yo'},
-            {id: 5, message: 'Yo'},
-            {id: 6, message: 'Yo'}
-        ],
-        dialogsData: [
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Andrey'},
-            {id: 3, name: 'Sveta'},
-            {id: 4, name: 'Sasha'},
-            {id: 5, name: 'Viktor'},
-            {id: 6, name: 'Valera'},
-            {id: 7, name: 'Umit'},
-        ]
+    _callSubscriber() {
+        console.log('state was changed');
+    },
+    getState(){
+        return this._state;
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state)
+    },
+    addMessage() {
+        let newMessage = {
+            id: 7,
+            message: this._state.dialogsPage.newMessageText
+        };
+        this._state.dialogsPage.messagesData.push(newMessage);
+        this._state.dialogsPage.newMessageText='';
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageText(newText) {
+        this._state.dialogsPage.newMessageText = newText;
+        this._callSubscriber(this._state)
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer; //observer // publisher-subscriber //наблюдатель//addEventListener // degisim gozetleyicisi
     }
-};
-window.state = state;
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rendererEntireTree(state);
-};
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rendererEntireTree(state)
-};
-export const addMessage = (addMessages) => {
-    let newMessage = {
-        id: 7,
-        message: addMessages
-    };
-    state.dialogsPage.messagesData.push(newMessage);
-    rendererEntireTree(state);
-};
-export const subscribe = (observer) => {
-    rendererEntireTree = observer; //observer // publisher-subscriber //наблюдатель//addEventListener // degisim gozetleyicisi
 }
-export default state;
 
-//store - OOP ogrenecegiz!!
+export default store;
+window.store=store;
