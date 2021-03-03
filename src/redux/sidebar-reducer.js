2628030e1a748917d578051507c94e84d550b6d7
backspace1990 <umut90asan@gmail.com>
2021-03-03 13:25:52 +0300

    redux a gecmeden bir onceki adimi gerceklestirdim. dispatch kismi ayri ayri reduser kismina eklendi state ye importlandi

---
+++ src/redux/sidebar-reducer.js
@@ -0,0 +1,11 @@
+const SIDE_BAR='SIDE-BAR'
+const sidebarReducer=(state,action)=>{
+    switch (action.type){
+        case SIDE_BAR:
+            return state;
+        default:
+            return state;
+    }
+    return state;
+};
+export default sidebarReducer;
