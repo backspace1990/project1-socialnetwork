7c9ed64dfc2fcb74dc2a02d60dcd4b17d5e9faba
backspace1990 <umut90asan@gmail.com>
2021-03-03 13:24:14 +0300

    dispatch taki kisim reducer olarak ayri ayri duzenlendi

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,7 @@
+import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogs-reducer";
+import sidebarReducer from "./sidebar-reducer";
+
 let store = {
     _state: {
         profilePage: {
@@ -10,16 +14,6 @@ let store = {
             newPostText: '',
         },
         dialogsPage: {
-            messagesData: [
-                {id: 1, message: 'Hi'},
-                {id: 2, message: 'How is your it-kamasutra?'},
-                {id: 3, message: 'Yo'},
-                {id: 4, message: 'Yo'},
-                {id: 5, message: 'Yo'},
-                {id: 6, message: 'Yo'}
-            ],
-            newMessageText:''
-            ,
             dialogsData: [
                 {id: 1, name: 'Dimych'},
                 {id: 2, name: 'Andrey'},
@@ -28,8 +22,19 @@ let store = {
                 {id: 5, name: 'Viktor'},
                 {id: 6, name: 'Valera'},
                 {id: 7, name: 'Umit'},
-            ]
-        }
+            ],
+            messagesData: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How is your it-kamasutra?'},
+                {id: 3, message: 'Yo'},
+                {id: 4, message: 'Yo'},
+                {id: 5, message: 'Yo'},
+                {id: 6, message: 'Yo'}
+            ],
+            newMessageText:'',
+
+        },
+        sidebar:{}
     },
     _callSubscriber() {
         console.log('state was changed');
@@ -42,32 +47,10 @@ let store = {
         this._callSubscriber = observer; //observer // publisher-subscriber //наблюдатель//addEventListener // degisim gozetleyicisi
     },
     dispatch(action){ //{type:'ADD-POST'}
-        if(action.type==='ADD-POST'){
-            let newPost = {
-                id: 5,
-                message: this._state.profilePage.newPostText,
-                likesCount: 0
-            };
-            this._state.profilePage.posts.push(newPost);
-            this._state.profilePage.newPostText = '';
-            this._callSubscriber(this._state);
-        }else if(action.type==='UPDATE-NEW-POST-TEXT'){
-            this._state.profilePage.newPostText = action.newText;
-            this._callSubscriber(this._state)
-        }else if(action.type==='ADD-MESSAGE'){
-            let newMessage = {
-                id: 7,
-                message: this._state.dialogsPage.newMessageText,
-            };
-            this._state.dialogsPage.messagesData.push(newMessage);
-            this._state.dialogsPage.newMessageText = '';
-            this._callSubscriber(this._state);
-        }  else if(action.type==='UPDATE-NEW-MESSAGE-TEXT'){
-            this._state.dialogsPage.newMessageText = action.newText;
-            this._callSubscriber(this._state);
-        }
-
-
+        this._state.profilePage=profileReducer(this._state.profilePage,action);
+        this._state.dialogsPage=dialogsReducer(this._state.dialogsPage,action);
+        this._state.sidebar=sidebarReducer(this._state.sidebar,action);
+        this._callSubscriber(this._state)
     }
 
 }
