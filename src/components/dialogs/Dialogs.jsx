6e1cf106d11c7123c91473416bf3d595eab7b87a
backspace1990 <umut90asan@gmail.com>
2021-03-03 13:27:49 +0300

    redux a gecmeden bir onceki adimi gerceklestirdim. Mentor ile ayni sekle getirildi duzenlendi.

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import classes from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import {addMessageActionCreator,updateNewMessageTextActionCreator} from "../../redux/dialogs-reducer";
 
 const Dialogs = (props) => {
     let dialogsElements = props.state.dialogsData.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>);
     let messagesElements = props.state.messagesData.map(message => <Message message={message.message}/>);
     let newMessageElement = React.createRef();/*onemli burasi link olusturduk*/
     let addMessage = ()=>{
-        props.dispatch({type:'ADD-MESSAGE'});
+        props.dispatch(addMessageActionCreator());
     };
     let onMessageChange=()=>{
         let text=newMessageElement.current.value;
-        props.dispatch({type:'UPDATE-NEW-MESSAGE-TEXT', newText:text});
+        props.dispatch(updateNewMessageTextActionCreator(text));
     };
 
     return (
@@ -24,7 +25,7 @@ const Dialogs = (props) => {
             <div className={classes.messages}>
                 {messagesElements}
                 <div>
-                    <textarea onChange={onMessageChange} ref={newMessageElement} value={props.newMessageText}></textarea>
+                    <textarea onChange={onMessageChange} ref={newMessageElement} value= {props.newMessageText} placeholder='Enter your message'></textarea>
                     <div>
                         <button onClick={addMessage}>Add Message</button>
                     </div>
--- src/components/profile/MyPosts/MyPosts.jsx
+++ src/components/profile/MyPosts/MyPosts.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import classes from './MyPosts.module.css';
 import Post from "./Post/Post";
+import {addPostActionCreator,updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 
 const MyPosts = (props) => {
     let postsElements = props.posts.map(post => <Post message={post.message} like={post.likesCount}/>
     );
     let newPostElement = React.createRef();
     let addPost = () => {
-        props.dispatch({type:'ADD-POST'});
+        props.dispatch(addPostActionCreator());
     };
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        let action = {type:'UPDATE-NEW-POST-TEXT',newText:text};
+        let action = updateNewPostTextActionCreator(text);
         props.dispatch(action);
 
 
@@ -21,7 +22,7 @@ const MyPosts = (props) => {
             <h3>My post</h3>
             <div>
                 <div>
-                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}/>
+                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} placeholder='Enter your post'/>
                 </div>
                 <div>
                     <button onClick={addPost}>Add post</button>
