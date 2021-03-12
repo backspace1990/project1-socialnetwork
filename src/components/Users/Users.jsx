dc6bd8b159c9825a296ce6e31308557c26e42439
backspace1990 <umut90asan@gmail.com>
2021-03-12 20:06:23 +0300

    class components kismi duzenlendi.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -47,43 +47,6 @@ class Users extends React.Component {
         </div>
     }
 };
-/*let
-Users = (props) => {
-    let getUsers = () => {
 
-    }
-    return <div>
-        <button onClick={getUsers}>Get Users</button>
-        {
-            props.users.map(u => <div key={u.id}>
-            <span>
-                <div>
-                    <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
-                </div>
-                <div>
-                    {u.followed
-                        ? <button onClick={() => {
-                            props.unfollow(u.id)
-                        }}>Unfollow</button>
-                        : <button onClick={() => {
-                            props.follow(u.id)
-                        }}>Follow</button>}
-
-                </div>
-            </span>
-                <span>
-                <span>
-                    <div>{u.name}</div>
-                    <div>{u.status}</div>
-                </span>
-                <span>
-                    <div>{"u.location.country"}</div>
-                    <div>{"u.location.city"}</div>
-                </span>
-            </span>
-            </div>)
-        }
-    </div>
-}*/
 
 export default Users;
