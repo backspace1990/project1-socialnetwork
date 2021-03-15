ab0ed0adec2a438c5d81557cd38f55aafbd46e98
backspace1990 <umut90asan@gmail.com>
2021-03-15 21:59:57 +0300

    mapDispatchToProps kismi connect fonk. object olarak eklendi

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {
-    followAC,
-    serUsersTotalCountAC,
-    setCurrentPageAC,
-    setIsFetchingAC,
-    setUsersAC,
-    unfollowAC
+    follow,
+    unfollow,
+    setUsers,
+    setCurrentPage,
+    setTotalUsersCount,
+    toggleIsFetching
 } from '../../redux/users-reducer';
 import * as axios from 'axios';
 import Users from "./Users";
@@ -22,7 +22,8 @@ class UsersContainer extends React.Component {
                 this.props.setTotalUsersCount(response.data.totalCount)
             });
     }
-    onPageChanged = (pageNumer)=>{
+
+    onPageChanged = (pageNumer) => {
         this.props.setCurrentPage(pageNumer);
         this.props.toggleIsFetching(true);
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumer}&count=${this.props.pageSize}`)
@@ -31,17 +32,18 @@ class UsersContainer extends React.Component {
                 this.props.setUsers(response.data.items)
             });
     }
+
     render() {
 
         return <>
             {this.props.isFetching ? <Preloader/> : null}
             <Users totalUsersCount={this.props.totalUsersCount}
-                      pageSize={this.props.pageSize}
-                      currentPage={this.props.currentPage}
-                      onPageChanged={this.onPageChanged}
-                      users={this.props.users}
-                      unfollow={this.props.unfollow}
-                      follow={this.props.follow}
+                   pageSize={this.props.pageSize}
+                   currentPage={this.props.currentPage}
+                   onPageChanged={this.onPageChanged}
+                   users={this.props.users}
+                   unfollow={this.props.unfollow}
+                   follow={this.props.follow}
             />
         </>
     }
@@ -50,7 +52,7 @@ class UsersContainer extends React.Component {
 let mapStateToProps = (state) => {
     return {
         users: state.usersPage.users,
-        pageSize:state.usersPage.pageSize,
+        pageSize: state.usersPage.pageSize,
         totalUsersCount: state.usersPage.totalUsersCount,
         currentPage: state.usersPage.currentPage,
         isFetching: state.usersPage.isFetching
@@ -59,7 +61,7 @@ let mapStateToProps = (state) => {
     }
 }
 
-let mapDispatchToProps = (dispatch) => {
+/*let mapDispatchToProps = (dispatch) => {
     return {
         follow: (userId) => {
             dispatch(followAC(userId));
@@ -81,6 +83,13 @@ let mapDispatchToProps = (dispatch) => {
         }
 
     }
-}
+}*/
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
+export default connect(mapStateToProps, {
+    follow,
+    unfollow,
+    setUsers,
+    setCurrentPage,
+    setTotalUsersCount,
+    toggleIsFetching
+})(UsersContainer);
